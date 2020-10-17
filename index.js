8652e10e8c30ff7bf67dd05f657ae5f716329dc4
Andrew Chan <dowinterfor6@gmail.com>
2020-10-17 16:59:24 -0700

    Attempt to connect botkit with express

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
-  // }
+}
 app.use(bodyParser.json());
 
 let botkitConfig = {
@@ -44,6 +44,8 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+controller.webserver = app;
 
 // Once the bot has booted up its internal services, you can use them to do stuff.
 controller.ready(() => {
