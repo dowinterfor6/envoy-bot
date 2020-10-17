65119b6b1f286c1042a0f5e89832f22bb7636c78
Andrew Chan <dowinterfor6@gmail.com>
2020-10-17 15:05:08 -0700

    Attempt heroku fix

--- index.js
+++ index.js
@@ -15,50 +15,50 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
-}
+  // }
+app.use(bodyParser.json());
 
-let botkitConfig = {
-  webhook_uri: '/api/messages',
-  adapter: adapter,
-  storage: null
-};
+// let botkitConfig = {
+//   webhook_uri: '/api/messages',
+//   adapter: adapter,
+//   storage: null
+// };
 
-const controller = new Botkit(botkitConfig);
+// const controller = new Botkit(botkitConfig);
 
 // TODO: Currently not being used
-if (process.env.CMS_URI) {
-  controller.usePlugin(new BotkitCMSHelper({
-    uri: process.env.CMS_URI,
-    token: process.env.CMS_TOKEN,
-  }));
-}
+// if (process.env.CMS_URI) {
+//   controller.usePlugin(new BotkitCMSHelper({
+//     uri: process.env.CMS_URI,
+//     token: process.env.CMS_TOKEN,
+//   }));
+// }
 
-// Once the bot has booted up its internal services, you can use them to do stuff.
-controller.ready(() => {
+// // Once the bot has booted up its internal services, you can use them to do stuff.
+// controller.ready(() => {
 
-  // load traditional developer-created local custom feature modules
-  controller.loadModules(__dirname + '/features');
+//   // load traditional developer-created local custom feature modules
+//   controller.loadModules(__dirname + '/features');
 
-  /* catch-all that uses the CMS to trigger dialogs */
-  if (controller.plugins.cms) {
-    controller.on('message,direct_message', async (bot, message) => {
-      let results = false;
-      results = await controller.plugins.cms.testTrigger(bot, message);
+//   /* catch-all that uses the CMS to trigger dialogs */
+//   if (controller.plugins.cms) {
+//     controller.on('message,direct_message', async (bot, message) => {
+//       let results = false;
+//       results = await controller.plugins.cms.testTrigger(bot, message);
 
-      if (results !== false) {
-        // do not continue middleware!
-        return false;
-      }
-    });
-  }
-});
+//       if (results !== false) {
+//         // do not continue middleware!
+//         return false;
+//       }
+//     });
+//   }
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
