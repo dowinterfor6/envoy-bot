ea944a912cb7a4a34f8b10920e05e6fb8383912c
Andrew Chan <dowinterfor6@gmail.com>
2020-10-17 13:34:06 -0700

    Fix scroll issue, fix overflow issue

--- frontend/src/components/EnvoyBot/EnvoyBot.jsx
+++ frontend/src/components/EnvoyBot/EnvoyBot.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { useEffect } from 'react';
 import { postMessage } from '../../utils/bot_utils';
 import BotNetworkStatus from './BotNetworkStatus';
 
@@ -6,6 +7,11 @@ const EnvoyBot = () => {
   const currUser = "69420";
   const [textInput, setTextInput] = useState("");
   const [messages, setMessages] = useState([]);
+  const messageListRef = useRef(null);
+
+  useEffect(() => {
+    messageListRef.current.scrollTop = messageListRef.current.scrollHeight - messageListRef.current.clientHeight;
+  }, [messages]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,28 +21,33 @@ const EnvoyBot = () => {
       channel: "websocket",
       user: currUser
     }
-    console.log(payload)
     // TODO: Useful for if there's a significant difference between message sent and response?
     setMessages([...messages, payload]);
+    sendMessage(payload);
+    setTextInput("");
+  };
+
+  const sendMessage = async (payload) => {
     // TODO: Handle unsuccessful message?
     // TODO: Handle typing/loading?
     // TODO: Lock further input to prevent spam/data getting lost?
     const { data } = await postMessage(payload);
     // TODO: Probably try to optimize this?
     setMessages([...messages, payload, ...data]);
-    setTextInput("");
-  };
+  }
 
   return (
     <section className="envoy-bot-container">
       {/* TODO: Make this into a generic header */}
       <BotNetworkStatus />
-      <ul className="message-list">
+      <ul className="message-list" ref={messageListRef}>
         {messages.map(({ type, text, user }, idx) => {
           return (
             <li key={`${type}-${idx}`} className={user === currUser ? "user" : "bot"}>
               <div className="message">
-                {text}
+                <p>
+                  {text}
+                </p>
               </div>
             </li>
           )
